f6c3cdb7f640849bd9d1fa63a0e140c8f66547b8
jinsim <simsime@korea.ac.kr>
2022-09-18 20:51:40 +0900

    첫 커밋

---
+++ src/test/java/com/jinsim/springboilerplate/SpringBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinsim.springboilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoilerplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
