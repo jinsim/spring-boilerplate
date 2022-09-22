c81723acde195312defda031090c42e0c543bf5a
jinsim <simsime@korea.ac.kr>
2022-09-22 12:03:42 +0900

    회원 CRUD 테스트

--- src/test/java/com/jinsim/springboilerplate/Account/service/AccountServiceTest.java
+++ src/test/java/com/jinsim/springboilerplate/Account/service/AccountServiceTest.java
@@ -2,6 +2,7 @@ package com.jinsim.springboilerplate.Account.service;
 
 import com.jinsim.springboilerplate.Account.domain.Account;
 import com.jinsim.springboilerplate.Account.dto.SignupReqDto;
+import com.jinsim.springboilerplate.Account.dto.UpdateAccountReqDto;
 import com.jinsim.springboilerplate.Account.repository.AccountRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,9 +16,11 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
-public class AccountServiceTest {
+class AccountServiceTest {
 
     @InjectMocks
     private AccountService accountService;
@@ -26,7 +29,7 @@ public class AccountServiceTest {
     private AccountRepository accountRepository;
 
     @Test
-    public void 회원가입_성공() {
+    void 회원가입_성공() {
         // given
         SignupReqDto requestDto = buildSignupReqDto();
         Account account = requestDto.toEntity();
@@ -41,10 +44,47 @@ public class AccountServiceTest {
         Long findAccountId = accountService.signup(requestDto);
 
         // then
-        System.out.println("findAccountId = " + findAccountId);
         Account findAccount = accountRepository.findById(findAccountId).get();
         assertEquals(account.getEmail(), findAccount.getEmail());
         assertEquals(account.getName(), findAccount.getName());
+    }
+
+    @Test
+    void 회원수정_성공() {
+        // given
+        UpdateAccountReqDto requestDto = buildUpdateAccountReqDto();
+        Account newAccount = requestDto.toEntity();
+        Account oldAccount = buildSignupReqDto().toEntity();
+        Long accountId = 1L;
+        ReflectionTestUtils.setField(oldAccount, "id", accountId);
+
+        // mock
+        given(accountRepository.findById(accountId)).willReturn(Optional.ofNullable(oldAccount));
+
+        // when
+        accountService.update(accountId, requestDto);
+
+        // then
+        Account findAccount = accountRepository.findById(accountId).get();
+        assertEquals(newAccount.getEmail(), findAccount.getEmail());
+        assertEquals(newAccount.getName(), findAccount.getName());
+    }
+
+    @Test
+    void 회원삭제_성공() {
+        // given
+        Account oldAccount = buildSignupReqDto().toEntity();
+        Long accountId = 1L;
+        ReflectionTestUtils.setField(oldAccount, "id", accountId);
+
+        // mock
+        given(accountRepository.findById(accountId)).willReturn(Optional.ofNullable(oldAccount));
+
+        // when
+        accountService.delete(accountId);
+
+        // then
+        verify(accountRepository, atLeastOnce()).delete(any(Account.class));
     }
 
     private SignupReqDto buildSignupReqDto() {
@@ -52,6 +92,13 @@ public class AccountServiceTest {
                 .email("test@email.com")
                 .name("회원가입테스트")
                 .password("pass123")
+                .build();
+    }
+
+    private UpdateAccountReqDto buildUpdateAccountReqDto() {
+        return UpdateAccountReqDto.builder()
+                .email("newtest@email.com")
+                .name("회원수정테스트")
                 .build();
     }
 
