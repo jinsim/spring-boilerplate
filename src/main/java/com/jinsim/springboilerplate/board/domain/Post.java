1a1119f3146b2ec000cbf44edd2fb3f6551074ea
jinsim <simsime@korea.ac.kr>
2022-10-17 20:43:40 +0900

    Post 클래스 추가

---
+++ src/main/java/com/jinsim/springboilerplate/board/domain/Post.java
@@ -0,0 +1,40 @@
+package com.jinsim.springboilerplate.board.domain;
+
+import com.jinsim.springboilerplate.account.domain.Account;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long id;
+
+    @NotNull(message = "제목은 필수로 입력되어야 합니다.")
+    @Column(length = 500)
+    private String title;
+
+    @NotNull(message = "내용은 필수로 입력되어야 합니다.")
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "account_id", updatable = false)
+    private Account account;
+
+    @Builder
+    public Post(String title, String content, Account account) {
+        this.title = title;
+        this.content = content;
+        this.account = account;
+    }
+}
