1c13b080cbc6aa9eef0c32dc2c08b4d45e7f6ac3
jinsim <simsime@korea.ac.kr>
2022-10-18 11:22:06 +0900

    Post repository test 추가

---
+++ src/test/java/com/jinsim/springboilerplate/board/repository/PostRepositoryTest.java
@@ -0,0 +1,81 @@
+package com.jinsim.springboilerplate.board.repository;
+
+import com.jinsim.springboilerplate.account.domain.Account;
+import com.jinsim.springboilerplate.account.repository.AccountRepository;
+import com.jinsim.springboilerplate.board.domain.Post;
+import com.jinsim.springboilerplate.global.test.RepositoryTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PostRepositoryTest extends RepositoryTest {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @BeforeEach
+    public void setUp() {
+        Account account1 = createAccount("test1@email.com", "test1", "encodedPassword1");
+        Account account2 = createAccount("test2@email.com", "test2", "encodedPassword2");
+
+        createPost("title1", "content1", account1);
+        createPost("title1", "content1", account1);
+        createPost("title3", "content3", account1);
+        createPost("title4", "content4", account2);
+        createPost("title5", "content5", account2);
+    }
+
+    @Test
+    void findAllByOrderByIdDescTest() {
+        List<Post> posts = postRepository.findAllByOrderByIdDesc();
+        for (Post post : posts) {
+            System.out.println("post.getTitle() = " + post.getTitle());
+        }
+        System.out.println("posts = " + posts);
+        assertThat(posts.size()).isEqualTo(5);
+    }
+
+    @Test
+    void findByAuthorTest() {
+        Account account = accountRepository.findByEmail("test1@email.com").get();
+        List<Post> posts = postRepository.findByAuthor(account);
+        assertThat(posts.size()).isEqualTo(3);
+    }
+
+    @Test
+    void findByTitleTest() {
+        String title = "title1";
+        List<Post> posts = postRepository.findByTitle(title);
+        assertThat(posts.size()).isEqualTo(2);
+    }
+
+    private Account createAccount(String email, String name, String encodedPassword) {
+        Account account = Account.builder()
+                .email(email)
+                .name(name)
+                .encodedPassword(encodedPassword)
+                .build();
+        return accountRepository.save(account);
+    }
+
+    private Post createPost(String title, String content, Account author) {
+        Post post = Post.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+        return postRepository.save(post);
+    }
+
+
+
+
+
+}
