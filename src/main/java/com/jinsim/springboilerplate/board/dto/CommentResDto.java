6c79e8081ba2fd1ddc85e0a94e3c33fa3f774977
jinsim <simsime@korea.ac.kr>
2022-10-21 14:44:45 +0900

    Comment DTO 생성

---
+++ src/main/java/com/jinsim/springboilerplate/board/dto/CommentResDto.java
@@ -0,0 +1,24 @@
+package com.jinsim.springboilerplate.board.dto;
+
+import com.jinsim.springboilerplate.board.domain.Comment;
+import com.jinsim.springboilerplate.board.domain.Post;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class CommentResDto {
+
+    private Long postId;
+    private String content;
+
+    public static CommentResDto of(Comment comment) {
+        return CommentResDto.builder()
+                .postId(comment.getPost().getId())
+                .content(comment.getContent())
+                .build();
+    }
+}
