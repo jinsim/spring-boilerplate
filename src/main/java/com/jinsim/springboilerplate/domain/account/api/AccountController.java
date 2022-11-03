bfda5fb2a119b6d4df4a5b04c43d20f28da22911
jinsim <simsime@korea.ac.kr>
2022-11-03 11:49:23 +0900

    swagger API 반영

--- src/main/java/com/jinsim/springboilerplate/domain/account/api/AccountController.java
+++ src/main/java/com/jinsim/springboilerplate/domain/account/api/AccountController.java
@@ -5,6 +5,10 @@ import com.jinsim.springboilerplate.domain.account.dto.SignUpReqDto;
 import com.jinsim.springboilerplate.domain.account.service.AccountService;
 import com.jinsim.springboilerplate.domain.account.dto.MyAccountResDto;
 import com.jinsim.springboilerplate.domain.account.dto.UpdateAccountReqDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -13,14 +17,19 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@Tag(name = "Account API", description = "계정 API")
 @Slf4j
-@RestController // @Controller + @ResponseBody
+@RestController
 @RequestMapping("/accounts")
-@RequiredArgsConstructor // final이 붙은 필드에 대해 생성자를 만들어준다.
+@RequiredArgsConstructor
 public class AccountController {
 
     private final AccountService accountService;
 
+    @Operation(summary = "회원가입", description = "계정을 생성합니다.")
+    @Parameters({
+            @Parameter(name = "requestDto", description = "회원가입 요청 객체"),
+    })
     @PostMapping
     @ResponseStatus(value = HttpStatus.CREATED)
     public MyAccountResDto signUp(@RequestBody @Valid final SignUpReqDto requestDto) {
@@ -30,26 +39,39 @@ public class AccountController {
     }
 
 
-    @GetMapping("/{id}")
-    @PreAuthorize("isAuthenticated() and (( @accountService.findById(#id).getEmail() == principal.username ) or hasRole('ROLE_ADMIN'))")
-    public MyAccountResDto getMyAccount(@PathVariable final Long id) {
-        Account account = accountService.findById(id);
+    @Operation(summary = "회원조회", description = "계정 정보를 조회합니다.")
+    @Parameters({
+            @Parameter(name = "accountId", description = "계정 아이디", example = "1")
+    })
+    @GetMapping("/{accountId}")
+    @PreAuthorize("isAuthenticated() and (( @accountService.findById(#accountId).getEmail() == principal.username ) or hasRole('ROLE_ADMIN'))")
+    public MyAccountResDto getMyAccount(@PathVariable final Long accountId) {
+        Account account = accountService.findById(accountId);
         return new MyAccountResDto(account);
     }
 
-    @PutMapping("/{id}")
-    @PreAuthorize("isAuthenticated() and (( @accountService.findById(#id).getEmail() == principal.username ) or hasRole('ROLE_ADMIN'))")
-    public MyAccountResDto updateMyAccount(@PathVariable final Long id,
+    @Operation(summary = "회원수정", description = "계정 정보를 수정합니다.")
+    @Parameters({
+            @Parameter(name = "accountId", description = "계정 아이디", example = "1"),
+            @Parameter(name = "requestDto", description = "회원수정 요청 객체")
+    })
+    @PutMapping("/{accountId}")
+    @PreAuthorize("isAuthenticated() and (( @accountService.findById(#accountId).getEmail() == principal.username ) or hasRole('ROLE_ADMIN'))")
+    public MyAccountResDto updateMyAccount(@PathVariable final Long accountId,
                                            @RequestBody final UpdateAccountReqDto requestDto) {
-        accountService.update(id, requestDto);
-        Account account = accountService.findById(id);
+        accountService.update(accountId, requestDto);
+        Account account = accountService.findById(accountId);
         return new MyAccountResDto(account);
     }
 
-    @DeleteMapping("/{id}")
-    @PreAuthorize("isAuthenticated() and (( @accountService.findById(#id).getEmail() == principal.username ) or hasRole('ROLE_ADMIN'))")
+    @Operation(summary = "회원삭제", description = "계정을 삭제합니다.")
+    @Parameters({
+            @Parameter(name = "accountId", description = "계정 아이디", example = "1")
+    })
+    @DeleteMapping("/{accountId}")
+    @PreAuthorize("isAuthenticated() and (( @accountService.findById(#accountId).getEmail() == principal.username ) or hasRole('ROLE_ADMIN'))")
     @ResponseStatus(value = HttpStatus.OK)
-    public void deleteMyAccount(@PathVariable final Long id) {
-        accountService.delete(id);
+    public void deleteMyAccount(@PathVariable final Long accountId) {
+        accountService.delete(accountId);
     }
 }
