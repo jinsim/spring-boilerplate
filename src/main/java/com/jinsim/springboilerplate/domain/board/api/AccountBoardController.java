c2db5fc03a6b288929ec73ae7fb764bda0174fb9
jinsim <simsime@korea.ac.kr>
2022-11-08 16:38:33 +0900

    Swagger Account Board API 추가

--- src/main/java/com/jinsim/springboilerplate/domain/board/api/AccountBoardController.java
+++ src/main/java/com/jinsim/springboilerplate/domain/board/api/AccountBoardController.java
@@ -8,6 +8,10 @@ import com.jinsim.springboilerplate.domain.board.service.CommentService;
 import com.jinsim.springboilerplate.domain.board.service.PostService;
 import com.jinsim.springboilerplate.domain.board.dto.CommentListResDto;
 import com.jinsim.springboilerplate.domain.board.dto.PostListResDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -16,6 +20,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Account Board API", description = "회원 게시판 API")
 @Slf4j
 @RestController
 @RequestMapping("/accounts/{accountId}")
@@ -25,6 +30,10 @@ public class AccountBoardController {
     private final CommentService commentService;
     private final PostService postService;
 
+    @Operation(summary = "댓글 목록 조회", description = "특정 회원이 작성한 댓글 목록을 조회합니다.")
+    @Parameters({
+            @Parameter(name = "accountId", description = "회원 아이디", example = "1")
+    })
     @GetMapping("/comments")
     @PreAuthorize("isAuthenticated() and (( @accountService.findById(#accountId).getEmail() == principal.username ) or hasRole('ROLE_ADMIN'))")
     @ResponseStatus(value = HttpStatus.OK)
@@ -33,6 +42,10 @@ public class AccountBoardController {
         return CommentListResDto.Account.of(account.getName(), commentList);
     }
 
+    @Operation(summary = "게시글 목록 조회", description = "특정 회원이 작성한 게시글 목록을 조회합니다.")
+    @Parameters({
+            @Parameter(name = "accountId", description = "회원 아이디", example = "1")
+    })
     @GetMapping("/posts")
     @PreAuthorize("isAuthenticated() and (( @accountService.findById(#accountId).getEmail() == principal.username ) or hasRole('ROLE_ADMIN'))")
     @ResponseStatus(value = HttpStatus.OK)
