851a6bc587e67de4dfbbe0f0842e847d2ad11a56
jinsim <simsime@korea.ac.kr>
2022-11-10 14:44:20 +0900

    Test에 profile 분리 적용

--- src/test/java/com/jinsim/springboilerplate/SpringBoilerplateApplicationTests.java
+++ src/test/java/com/jinsim/springboilerplate/SpringBoilerplateApplicationTests.java
@@ -2,8 +2,10 @@ package com.jinsim.springboilerplate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class SpringBoilerplateApplicationTests {
 
 	@Test
